5a5ee093989d6b7b93a38ca9e44cd17619f9e24c
Alex <alessandro.piccione.75@gmail.com>
2026-09-13 12:01:03 +0200

    add practice with LINQ

---
+++ Certify/Basic_Employee.cs
@@ -0,0 +1,16 @@
+﻿namespace HackerRank.Certify
+{
+    internal sealed record Employee(string Company, string FirstName, string LastName, int Age);
+
+    internal class Basic_Employee
+    {
+        public Dictionary<string, int> GetAverageAgeOfEmployeesPerCompany(List<Employee> employees) => employees.GroupBy(e => e.Company)
+                .ToDictionary(g => g.Key, g => (int)g.Average(e => e.Age));
+
+        public Dictionary<string, int> GetCountOfEmployeesPerCompany(List<Employee> employees) => employees.GroupBy(e => e.Company)
+                .ToDictionary(g => g.Key, g => g.Count());
+
+        public Dictionary<string, int> GetAgeOfOldestEmployeePerCompany(List<Employee> employees) => employees.GroupBy(e => e.Company)
+                .ToDictionary(g => g.Key, g => g.Max(e => e.Age));
+    }
+}
