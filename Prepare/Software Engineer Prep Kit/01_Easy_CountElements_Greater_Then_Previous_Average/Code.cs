3684fc6519c87b2b83686101ab36d9bb1f5cabc2
Alex <alessandro.piccione.75@gmail.com>
2026-09-13 12:07:47 +0200

    add exercise

---
+++ Prepare/Software Engineer Prep Kit/01_Easy_CountElements_Greater_Then_Previous_Average/Code.cs
@@ -0,0 +1,54 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+namespace HackerRank.Prepare.Software_Engineer_Prep_Kit._01_Easy_CountElements_Greater_Then_Previous_Average
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'countResponseTimeRegressions' function below.
+         *
+         * The function is expected to return an INTEGER.
+         * The function accepts INTEGER_ARRAY responseTimes as parameter.
+         */
+
+        public static int CountResponseTimeRegressions(List<int> responseTimes)
+        {
+
+        }
+
+    }
+
+    class Solution
+    {
+        public static void Main(string[] args)
+        {
+            int responseTimesCount = Convert.ToInt32(Console.ReadLine()??"".Trim());
+
+            List<int> responseTimes = new List<int>();
+
+            for (int i = 0; i < responseTimesCount; i++)
+            {
+                int responseTimesItem = Convert.ToInt32(Console.ReadLine()??"".Trim());
+                responseTimes.Add(responseTimesItem);
+            }
+
+            int result = Result.CountResponseTimeRegressions(responseTimes);
+
+            Console.WriteLine(result);
+        }
+    }
+
+}
